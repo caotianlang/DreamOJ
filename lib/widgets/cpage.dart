e35407bed7e3e0ee01e6ef40ce2de3dd5d3c7dc9
caotianlang <caotianlang@outlook.com>
2023-01-30 08:54:14 +0800

    add cpage

--- lib/widgets/cpage.dart
+++ lib/widgets/cpage.dart
@@ -32,3 +32,59 @@ class basicCPage extends StatelessWidget {
     );
   }
 }
+
+class cPage extends basicCPage {
+  final Widget mainWidget;
+  final Widget? leftWidget;
+  final Widget? rightWidget;
+  final MainAxisAlignment mainAxisAlignment;
+
+  const cPage({
+    super.key,
+    required this.mainWidget,
+    this.leftWidget,
+    this.rightWidget,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  }) : super(child: mainWidget);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Color.fromARGB(8, 0, 0, 0)),
+        child: Column(
+          children: [
+            TopBar(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Row(
+                    mainAxisAlignment: mainAxisAlignment,
+                    children: [
+                      Builder(builder: (context) {
+                        return leftWidget == null
+                            ? Spacer(flex: 2)
+                            : Expanded(flex: 2, child: leftWidget!);
+                      }),
+                      Expanded(
+                          flex: 10,
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: mainWidget,
+                          )),
+                      Builder(builder: (context) {
+                        return rightWidget == null
+                            ? Spacer(flex: 2)
+                            : Expanded(flex: 2, child: rightWidget!);
+                      }),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
