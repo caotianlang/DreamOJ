2d198b83c8af87bd6fcd943dc1a5b0b0dbbcedf4
caotianlang <caotianlang@outlook.com>
2023-01-30 13:40:35 +0800

    change route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,22 +1,30 @@
 import 'package:dreamoj/pages/unknown.dart';
+import 'package:dreamoj/widgets/cpage.dart';
 import 'package:flutter/material.dart';
 
 Route onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case '/':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     case '/login':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     case '/signup':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     case '/problems':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     case '/competition':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     case '/discuss':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     case '/store':
-      return MaterialPageRoute(builder: (_) => Container(), settings: settings);
+      return MaterialPageRoute(
+          builder: (_) => cPage(mainWidget: Container()), settings: settings);
     default:
       return MaterialPageRoute(
           builder: (_) => UnknownPage(), settings: settings);
